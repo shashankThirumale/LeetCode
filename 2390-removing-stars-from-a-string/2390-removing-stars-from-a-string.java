9a9875ffedc0bb3ec0ba762916d27b11ca1a1ad3
ShashankThirumale <92651426+ShashankThirumale@users.noreply.github.com>
2023-10-24 22:00:31 -0400

    Time: 29 ms (91.70%), Space: 43.8 MB (99.42%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.java
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.java
@@ -1,20 +1,14 @@
 class Solution {
     public String removeStars(String s) {
+        char[] chars = s.toCharArray();
         StringBuilder str = new StringBuilder();
-        for(int i = 0; i < s.length(); i++){
-            if(s.charAt(i) != '*'){
-                str.append(s.charAt(i));
+        for(int i = 0; i < chars.length; i++){
+            if(chars[i] != '*'){
+                str.append(chars[i]);
             }else{
                 str.deleteCharAt(str.length()-1);
             }
         }
         return str.toString();
     }
-    
-    /**
-    * We can create a stack that gets pushed all the characters before a star.
-    * Then pop out the rest of the elements and push them onto another stack
-    * then pop one at a time into a StringBuilder
-    * keep doing all of these steps until the end of the String
-    */
 }
