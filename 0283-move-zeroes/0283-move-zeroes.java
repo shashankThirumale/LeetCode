b49158fc2c8d851221c64c4bc3195d45b37ff5ea
ShashankThirumale <92651426+ShashankThirumale@users.noreply.github.com>
2023-10-28 08:56:39 -0400

    Time: 4 ms (17.05%), Space: 44.2 MB (99.10%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,15 +1,29 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int i = 0;
-        for(int num: nums){
-            if(num != 0){
-                nums[i] = num;
-                i++;
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i]!=0){
+                list.add(nums[i]);
             }
         }
-        while(i <= nums.length -1){
+        int i = 0;
+        for(i = 0; i < list.size(); i++){
+            nums[i] = list.get(i);
+        }
+        while(i < nums.length){
             nums[i] = 0;
             i++;
         }
     }
+    /**
+    Have an ArrayList with numbers from the list that are not zeros
+    Then we traverse through the ArrayList and and add them to the nums array starting from the first element
+    the rest of the numbers will be zeros
+    
+    
+    TestCases:
+    [0,1,0,3,12]
+    list = [1,3,12]
+    
+    */
 }
