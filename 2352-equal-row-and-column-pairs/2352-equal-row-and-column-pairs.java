d861c3398daa2f4501ff1af570b52a508c4016d0
ShashankThirumale <92651426+ShashankThirumale@users.noreply.github.com>
2023-10-28 19:05:01 -0400

    Time: 8 ms (94.16%), Space: 52.5 MB (18.25%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.java
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.java
@@ -1,31 +1,21 @@
 class Solution {
-    public int equalPairs(int[][] g) {
-		int n = g.length;
-        int res = 0;
-		//convert the numbers in each row to a single string
-        Map<String, Integer> sToFreq = new HashMap<>();
-        for (int i = 0; i < n; i++) {
-            StringBuilder sb = new StringBuilder();
-            for (int j = 0; j < n; j++) { 
-                sb.append(g[i][j]);
-                sb.append("+");
-            }
-            String s = sb.toString();
-            sToFreq.put(s, sToFreq.getOrDefault(s, 0) + 1);
-        }
-        
-		//convert the numbers in each col to string and find the matches
-        for (int j = 0; j < n; j++) {
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < n; i++) { 
-                sb.append(g[i][j]);
-                sb.append("+");
-            }
-            String s = sb.toString();
-            if (sToFreq.containsKey(s)) {
-                res += sToFreq.get(s);
+   public int equalPairs(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        int[][] transpose = new int[m][n];
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                //Transpose of matrix
+                transpose[j][i] = grid[i][j];
             }
         }
-        return res;
+        int ans = 0;
+        for(int[] row : grid){
+            for(int[] col : transpose){
+                //Check for equality
+                if(Arrays.equals(row, col)) ans++;
+            }
+        }
+        return ans;
     }
 }
