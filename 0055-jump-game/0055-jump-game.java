48f017d1d2320f4e2c4c9def93f220c260180d22
ShashankThirumale <92651426+ShashankThirumale@users.noreply.github.com>
2023-10-29 14:44:21 -0400

    Time: 2 ms (80.60%), Space: 45 MB (6.07%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,11 +1,15 @@
 class Solution {
      public boolean canJump(int[] nums) {
-        int boundary = 0;
-        for(int i =0;i<=boundary;i++){
-            boundary = Math.max(boundary,i+nums[i]);
-            if(boundary >=nums.length-1)
+       int maxReach = 0;
+        int n = nums.length;
+
+        for (int i = 0; i < n && i<=maxReach; i++) {
+            
+            maxReach = Math.max(maxReach, i + nums[i]);
+            if (maxReach >= n - 1) {
                 return true;
-        } 
+            }
+        }
         return false;
     }
     
