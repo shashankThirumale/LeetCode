6f5ae8fa207b737c341687ad3e7c36d83f22d54d
ShashankThirumale <92651426+ShashankThirumale@users.noreply.github.com>
2023-12-02 08:35:04 -0500

    Time: 5 ms (94.00%), Space: 44.5 MB (47.80%) - LeetHub

--- 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.java
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.java
@@ -1,24 +1,18 @@
 class Solution {
     public List<List<Integer>> groupThePeople(int[] groupSizes) {
+        List<List<Integer>> result = new LinkedList<>();
         HashMap<Integer, LinkedList<Integer>> map = new HashMap<>();
         for(int i = 0; i < groupSizes.length; i++){
             LinkedList<Integer> add = map.getOrDefault(groupSizes[i], new LinkedList<Integer>());
             add.add(i);
-            map.put(groupSizes[i], add);
-        }
-        List<List<Integer>> result = new LinkedList<>();
-        for(Integer key: map.keySet()){
-            LinkedList<Integer> remove = map.get(key);
-            while(!remove.isEmpty()){
-                Integer loop = key;
-                LinkedList<Integer> add = new LinkedList<>();
-                while(loop > 0){
-                    add.add(remove.remove());
-                    loop--;
-                }
+            if(add.size() == groupSizes[i]){
                 result.add(add);
+                map.remove(groupSizes[i]);
+            }else{
+                map.put(groupSizes[i], add);
             }
         }
+        
         return result;
     }
 }
