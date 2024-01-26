0ffcafc033f76cf9a673e2dc11e3661b93264a03
ShashankThirumale <92651426+ShashankThirumale@users.noreply.github.com>
2024-01-25 19:59:43 -0500

    Time: 4 ms (5.21%), Space: 44.6 MB (14.82%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -1,35 +1,22 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int index = 0;
-        int tracker = 0;
-        int currNum = nums[0];
+        LinkedHashMap<Integer, Integer> set = new LinkedHashMap<>();
         for(int i = 0; i < nums.length; i++){
-            if(nums[i] != currNum){
-                currNum = nums[i];
-                tracker = 1;
-                nums[index] = nums[i];
-                index++;
-            }else{
-                tracker++;
-                if(tracker < 3){
-                    nums[index] = nums[i];
-                    index++;
-                }
+            set.put(nums[i], set.getOrDefault(nums[i], 0)+1);
+        }
+        int i = 0;
+        int size = 0;
+        for(int num: set.keySet()){
+            int reps = set.get(num);
+            size++;
+            nums[i++] = num;
+            if(reps >= 2){
+                nums[i++] = num;
+                size++;
             }
         }
-        return index;
+        return size;
     }
     
-    /*
-     Questions:
-     - Can there be negative numbers? - Yes
-     - Can the array be empty? - no has to be atleast 1
-     
-     Possible solutions:
-     - A LinkedHashMap
-     - key would be the actual number. value would be the frequency.
-     - Then we can traverse through the LinkedHashMap in the order that we insert and change values 
-        in the array
-        
-    */
+    
 }
