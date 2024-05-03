ce332399c0833f76b5706a776771dfe4063c1c0a
ShashankThirumale <92651426+ShashankThirumale@users.noreply.github.com>
2024-05-03 11:38:47 -0400

    Time: 13 ms (33.49%), Space: 45.2 MB (67.00%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.java
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.java
@@ -1,33 +1,37 @@
 class Solution {
     public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
-        HashSet<Integer> set1 = new HashSet<>();
-        for(int i = 0; i < nums1.length; i++){
-            set1.add(nums1[i]);
+        ArrayList<Integer> list1 = new ArrayList<Integer>();
+        ArrayList<Integer> list2 = new ArrayList<Integer>();
+        
+        HashSet<Integer> num1 = new HashSet<>();
+        HashSet<Integer> num2 = new HashSet<>();
+        HashSet<Integer> done = new HashSet<>();
+        
+        for(int num: nums1){
+            num1.add(num);
         }
         
-        List<List<Integer>> result = new ArrayList<>();
-        result.add(null);
-        result.add(null);
-        HashSet<Integer> set2 = new HashSet<>();
-        ArrayList<Integer> sec = new ArrayList<>();
-        for(int i = 0; i < nums2.length; i++){
-            if(!set2.contains(nums2[i])){
-                set2.add(nums2[i]);
-                if(!set1.contains(nums2[i])){
-                    sec.add(nums2[i]);
-                }
-            }
+        for(int num: nums2){
+            num2.add(num);
         }
-        result.set(1, sec);
         
-        ArrayList<Integer> fir = new ArrayList<>();
-        for(int i = 0; i < nums1.length; i++){
-            if(!set2.contains(nums1[i]) && set1.contains(nums1[i])){
-                fir.add(nums1[i]);
-                set1.remove(nums1[i]);
+        for(int num: nums1){
+            if(!num2.contains(num) && !done.contains(num)){
+                list1.add(num);
+                done.add(num);
             }
         }
-        result.set(0, fir);
+        done = new HashSet<>();
+        for(int num: nums2){
+            if(!num1.contains(num) && !done.contains(num)){
+                list2.add(num);
+                done.add(num);
+            }
+        }
+        ArrayList<List<Integer>> result = new ArrayList<>();
+        result.add(list1);
+        result.add(list2);
+        
         return result;
     }
 }
