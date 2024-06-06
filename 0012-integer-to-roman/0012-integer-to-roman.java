2b519f987f89a4195bf5820cca0819ca15a3fe87
ShashankThirumale <92651426+ShashankThirumale@users.noreply.github.com>
2024-06-06 19:51:32 -0400

    Time: 3 ms (97.07%), Space: 43.9 MB (94.11%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -1,18 +1,32 @@
 class Solution {
     public String intToRoman(int num) {
-        int values[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-        String numerals[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-        
+        String map[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        int array[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
         StringBuilder sb = new StringBuilder();
-        int i = 0;
-        while(num != 0){
-            if(num/values[i] > 0){
-                sb.append(numerals[i]);
-                num -= values[i];
-            }else{
-                i++;
-            }
+        for(int i = 0; i < array.length; i++){
+            int total = num/array[i];
+            for(int j = 0; j < total; j++)
+                sb.append(map[i]);
+            num = num%array[i];
         }
         return sb.toString();
     }
+    /*
+    We need to covert the String to the corresponding Roman number
+    Plan
+        have an HashMap key = num value = Symbol
+        have an array with all of the possible values start with largest values
+    
+    psuedo
+    map
+    array
+    StringBuilder sb
+    for(int i = 0; i < array.length; i++){
+        total = num/array[i];
+        for(int j = 0; j < total; j++)
+            sb.append(map.get(array[i]));
+        num = num%1000;
+    }
+    return sb.toString();
+    */
 }
